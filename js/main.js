89ba4c6276b913a175179aa44c6d8faa9eddb6a7
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-16 15:16:04 +0900

    create graph about studying PG time

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@
 
   const type = 'line';
   const data = {
-    labels: [2010, 2011, 2012, 2013],
+    labels: ['2019/1', '2019/2', '2019/3', '2019/4'],
     datasets: [{
       label: 'taichiro',
       data: [120, 300, 200, 210]
     }, {
       label: 'ishin',
       data: [200, 150, 300, 750]
+    }, {
+      label: 'takao',
+      data: [150, 200, 300, 500]
     }]
   };
   const options = {
@@ -20,10 +23,18 @@
           suggestedMax: 400,
           stepSize: 100,
           callback: function(value, index, values) {
-            return 'JPY' + value;
+            return value + '時間';
           }
         }
       }]
+    },
+    title: {
+      display: true,
+      text: 'PG勉強時間',
+      fontsize: 18
+    },
+    legend: {
+      position: 'right'
     }
   };
 
